bf4e67a8b33f7274086dd6ccdf9045e8a5d74315
Manoel Eric <manuel@box3.work>
2026-05-29 14:15:20 -0300

    add env

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3333";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3333";
 
 function getHeaders(token?: string) {
 	const headers: Record<string, string> = {};
